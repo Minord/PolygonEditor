f8eadc4fdf02103f196b85d1849defc69d9fa5ca
Minord <benderrobot12@gmail.com>
2017-08-01 22:04:19 -0600

    Menu bar is created

--- src/Window.py
+++ src/Window.py
@@ -1,13 +1,24 @@
 import sys
-from PyQt5 import QtWidgets,QtGui
+from PyQt5 import QtWidgets, QtGui
+
+from PyQt5.QtWidgets import QWidget, QMainWindow, QAction
 
 var = 'Esto es una prueba para probrar los labels'
 name = 'PolygonEditor'
 
+#Tool Bar class
+class MenuBar(QWidget):
+    """ MenuBar."""
+    def __init__(self):
+        super(MenuBar, self).__init__()
+        pass
+
+
+
 #Creating Window
 def window():
     app = QtWidgets.QApplication(sys.argv)
-    w = QtWidgets.QWidget()
+    w = QMainWindow()
 
     w.setWindowTitle(name)
     w.setGeometry(100,100,750,450)
@@ -23,8 +34,21 @@ def window():
     label1.move(10,10)
     label2.move(50,70)
 
+    bar = w.menuBar()
+    file = bar.addMenu ('File')
+
+    any_action = QAction('Any', w)
+    file.addAction(any_action)
+
+    file.triggered.connect(respond)
 
     w.show()
     sys.exit(app.exec_())
+
+def respond(q):
+    signal = q.text()
+    if signal == 'Any':
+        print ('Hola')
+    pass
 
 window()
